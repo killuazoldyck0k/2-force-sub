feb35f975ed5fefbe2a230c6f35efe476b72fb85
killuazoldyck0k <149078023+killuazoldyck0k@users.noreply.github.com>
2023-10-27 11:49:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = os.environ.get("API_HASH", "")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "Killua_og")
+OWNER = os.environ.get("OWNER", "AryanTeamUniverse")
 
 # Protect Content
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
@@ -50,7 +50,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}\n\nʙᴀᴋᴋᴀᴀᴀ!! {first}\n\nI can provide files for @Anime_Alliance\nchannel Members❤️!!\n\nᴊᴜꜱᴛ ᴅᴏɴ'ᴛ ᴏᴠᴇʀʟᴏᴀᴅ ᴍᴇ 🫣.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5191566338").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "2098973647").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
@@ -68,7 +68,7 @@ DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "Fal
 
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((844432220, 5191566338))
+ADMINS.extend((844432220, 2098973647))
 
 
 LOG_FILE_NAME = "logs.txt"
